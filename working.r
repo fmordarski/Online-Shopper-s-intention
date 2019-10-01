e682133a72ea829c6fa4aaa2c6a438ea2b35f291
fmordarski <49123835+fmordarski@users.noreply.github.com>
2019-10-01 21:14:37 +0200

    Add files via upload

--- working.r
+++ working.r
@@ -9,7 +9,7 @@ if(!require(reshape2)) install.packages("data.table", repos = "http://cran.us.r-
 # Introduction
 
 ## provide below path to working directory
-setwd("C:/Users/filip-mordarski/Documents/filip/capstone/own project/Online-Shopper-s-intention-master")
+setwd("C:/Users/Uzytkownik/Documents/R courses edX/Harvard/capstone/own project")
 
 ##loading data
 data <- read.csv("online_shoppers_intention.csv",header=TRUE)
@@ -41,6 +41,73 @@ train_set <- data[-test_index, ]
 table_freq <- data %>%
   summarize(FreqPercTrue=mean(Revenue),FreqPercFalse=1-mean(Revenue)) 
 
+# pie chart
+
+bp <- ggplot(melt(table_freq), aes(x="",y=value,fill=variable)) +
+  geom_bar(width = 1, stat = "identity") +
+  ggtitle("Did the visitor generate revenue?")
+
+pie <- bp + coord_polar("y", start=0)
+
+# create blank theme
+blank_theme <- theme_minimal()+
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid=element_blank(),
+    axis.ticks = element_blank(),
+    plot.title=element_text(size=14, face="bold")
+  )
+#create nice chart
+
+pie + scale_fill_brewer("Legend", label = c(FreqPercTrue = "Yes", FreqPercFalse = "No")) + 
+  blank_theme +
+  theme(axis.text.x=element_blank()) 
+
+# Sort factor month in reverse order ("Feb","Mar","May","June","Jul","Aug","Sep","Oct","Nov","Dec") 
+
+data$Month = factor(data$Month,levels(data$Month)[c(2,8,9,10,1,4,5,7,6,3)])
+
+# Create plot and dataframe with counts of Revenue grouped by month
+
+data %>% ggplot(aes(Month))  + 
+  geom_bar(aes(fill = Revenue), position = position_stack(reverse = TRUE)) +
+  coord_flip() +
+  scale_fill_brewer("Revenue") +
+  theme(axis.text.x=element_blank()) +
+  ggtitle("Revenue counts by month") +
+  blank_theme 
+
+group_month <- data %>%
+  group_by(Month) %>%
+  summarise(True=sum(Revenue=='TRUE'),False=sum(Revenue=='FALSE'),Total=n()) 
+
+# Create plot and dataframe with counts of Revenue grouped by Visitor Type
+
+data %>% ggplot(aes(VisitorType))  + 
+  geom_bar(aes(fill = Revenue), position = position_stack(reverse = TRUE)) +
+  coord_flip() +
+  scale_fill_brewer("Revenue") +
+  theme(axis.text.x=element_blank()) +
+  ggtitle("Revenue counts by Visitortype") +
+  blank_theme 
+
+group_visitor <- data %>%
+  group_by(VisitorType) %>%
+  summarise(True=sum(Revenue=='TRUE'),False=sum(Revenue=='FALSE'),FreqPercTrue=round(sum(Revenue=='TRUE')/n(),2),Total=n()) 
+
+# Create dataframe with freq perc of revenue group by special day value
+
+special_day <- data %>%
+  group_by(SpecialDay) %>%
+  summarise(Counts=n(),FreqPerc=round(sum(Revenue=='TRUE')/n(),2))
+
+special_day %>% ggplot(aes(SpecialDay,FreqPerc)) +
+  geom_line(colour = "lightblue",size=2) +
+  ggtitle("Frequency Percentage True Revenue vs Special Day indicator") +
+  theme_minimal()
+  
 
 # train dataset with method Random Forest using caret package
 train_set$Revenue <- as.factor(train_set$Revenue)
